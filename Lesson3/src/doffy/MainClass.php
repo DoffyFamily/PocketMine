4d29f2c5b66524e7656182cd16e20293b2b16028
Donquih0te <Kb08ek@mail.ru>
2018-03-04 21:12:07 +0500

    Add files via upload
    Add Lesson 3

---
+++ Lesson3/src/doffy/MainClass.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace doffy;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+
+class MainClass extends PluginBase implements Listener{
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function interact(PlayerInteractEvent $event): void{
+        $player = $event->getPlayer();
+        $block = $event->getBlock();
+        $item = $event->getItem();
+        $action = $event->getAction();
+
+        if($action == PlayerInteractEvent::RIGHT_CLICK_BLOCK){
+            $player->sendMessage("Тап правой рукой по блоку");
+        }
+        if($item->getId() == 1){
+            $player->sendMessage("Там предметом с  id = 1");
+        }
+        if($block->getId() == 4){
+            $player->sendMessage("Тап по предмету с id = 4");
+        }
+    }
+
+}
+
